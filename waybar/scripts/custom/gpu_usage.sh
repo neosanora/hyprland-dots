f53d2b28bdc2d0e0378938a9245cf77535175be2
Neo arch <neosanora@gmail.com>
2025-07-31 08:08:01 +0000

    ahhhhh

--- waybar/scripts/custom/gpu_usage.sh
+++ waybar/scripts/custom/gpu_usage.sh
@@ -1,22 +1,5 @@
-# #!/usr/bin/env bash
+#!/usr/bin/env bash
 
-# # Deteksi GPU otomatis
-# if command -v nvidia-smi &>/dev/null; then
-#     # NVIDIA
-#     usage=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | head -n 1)
-#     echo "${usage}%"
-# elif command -v rocm-smi &>/dev/null; then
-#     # AMD ROCm
-#     usage=$(rocm-smi --showuse | grep -m 1 "%" | awk '{print $2}')
-#     echo "${usage}%"
-# elif command -v intel_gpu_top &>/dev/null; then
-#     # Intel (butuh sudo / setuid intel_gpu_top)
-#     usage=$(timeout 0.2 intel_gpu_top -J | jq '.engines.render.busy' | head -n 1)
-#     echo "${usage}%"
-# else
-#     echo "N/A"
-# fi
-#!/bin/bash
 
 # Gunakan nvtop JSON backend jika tersedia
 if command -v nvtop &>/dev/null; then
