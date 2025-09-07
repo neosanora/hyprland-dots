9b431455ead80f991c751070bfeb3c229ba4b43d
neosanora <neosanora@gmail.com>
2025-09-07 23:30:23 +0700

    Update aur-pakage.sh

--- setup/share/aur/aur-pakage.sh
+++ setup/share/aur/aur-pakage.sh
@@ -2,7 +2,7 @@
 # aur Packages
 # ----------------------------------------------------------
 
-Packages=(
+aur_packages=(
   "python-screeninfo"      # Python library to fetch location and size of physical screens
   "waypaper-git"           # Dynamic wallpaper changer for Wayland
   "grimblast-git"          # A helper for screenshots within Hyprland.
