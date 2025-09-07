1b863d5e048b3febda9c79e13d6c1ffcff541af3
neosanora <neosanora@gmail.com>
2025-09-07 15:50:39 +0700

    Update aur-pakage.sh

--- setup/share/aur/aur-pakage.sh
+++ setup/share/aur/aur-pakage.sh
@@ -5,4 +5,5 @@
 Packages=(
   "waypaper-git"           # Dynamic wallpaper changer for Wayland
   "grimblast-git"          # A helper for screenshots within Hyprland.
+  "hyprshade"              # Hyprland shade configuration tool
 )
