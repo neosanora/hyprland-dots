be3280f93fe850e65e4a85f8ebc07a86dd023ef1
neosanora <neosanora@gmail.com>
2025-09-10 12:18:43 +0000

    update

--- dotfiles/.config/hypr/scripts/volume.sh
+++ dotfiles/.config/hypr/scripts/volume.sh
@@ -1,28 +1,59 @@
 #!/usr/bin/env bash
 
-arg=$1
+STEP=3
+ICON_MAX="$HOME/.config/hypr/scripts/assets/volume-max.svg"
+ICON_MUTED="$HOME/.config/hypr/scripts/assets/muted.svg"
+ICON_ALERT="$HOME/.config/hypr/scripts/assets/alert.svg"
 
-if [ "$1" == "I" ]; then
-    pamixer -i 3
-elif [ "$1" == "D" ]; then
-    pamixer -d 3
-else
-    notify-send "Please use correct args" --icon=$HOME/.config/hypr/scripts/assets/alert.svg
-    exit
-fi
+# Ambil volume sekarang
+get_volume() {
+    pamixer --get-volume
+}
 
-volume=$(pamixer --get-volume)
+# Ambil status mute
+is_muted() {
+    pamixer --get-mute
+}
 
-if [ "$volume" == 100 ]; then
-    dunstify -h string:x-dunst-stack-tag:volume "Full Volume" --icon=$HOME/.config/hypr/scripts/assets/volume-max.svg
-    exit
-elif [ "$volume" == 0 ]; then
-    dunstify -h string:x-dunst-stack-tag:volume "No Volume" --icon=$HOME/.config/hypr/scripts/assets/muted.svg
-    exit
-else
-    dunstify -h string:x-dunst-stack-tag:volume -h int:value:$volume "Volume : $volume" --icon=$HOME/.config/hypr/scripts/assets/volume-max.svg
-    exit
-fi
+# Set notifikasi volume
+notify_volume() {
+    local vol=$1
+    local muted=$2
+    local icon="$ICON_MAX"
+    local title="󰕾 Volume"
 
-"Volume"
-exit
+    if [ "$muted" = "true" ] || [ "$vol" -eq 0 ]; then
+        title="󰝟 Muted"
+        icon="$ICON_MUTED"
+        vol=0
+    elif [ "$vol" -ge 100 ]; then
+        title=" Full Volume"
+        icon="$ICON_MAX"
+        vol=100
+    fi
+
+    notify-send -h int:value:"$vol" \
+                -h string:x-canonical-private-synchronous:volume \
+                -u low "$title" "$vol%" \
+                --icon="$icon"
+}
+
+case "$1" in
+    I)
+        pamixer -i "$STEP"
+        ;;
+    D)
+        pamixer -d "$STEP"
+        ;;
+    T)
+        pamixer -t
+        ;;
+    *)
+        notify-send "Please use correct args (I|D|T)" --icon="$ICON_ALERT"
+        exit 1
+        ;;
+esac
+
+volume=$(get_volume)
+muted=$(is_muted)
+notify_volume "$volume" "$muted"
