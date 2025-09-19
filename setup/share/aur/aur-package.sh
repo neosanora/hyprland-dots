d6b82c3b0879700ae581b101657a4a486d811332
neosanora-arch <neosanora@gmail.com>
2025-09-19 21:56:52 +0800

    update

--- setup/share/aur/aur-package.sh
+++ setup/share/aur/aur-package.sh
@@ -3,6 +3,7 @@
 # ----------------------------------------------------------
 
 aur_packages=(
+  "checkupdates-with-aur"  # Helper script to check updates, including AUR updates
   "python-screeninfo"      # Python library to fetch location and size of physical screens
   "waypaper-git"           # Dynamic wallpaper changer for Wayland
   "grimblast-git"          # A helper for screenshots within Hyprland.
